4a937588c77aa4779d5ba4c685871ad5b3220a14
Michael <hayesmv@yahoo.com>
2020-05-26 13:52:24 -0400

    server deploy

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize('DreamCatcher', 'postgres', process.env.PG_PASS, {
-    host: "localhost",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres"
 })
 
